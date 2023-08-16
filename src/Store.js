57b66e0fae008d37c646e144d5e21925964a1fdb
Bryant <bryant-m@hotmail.com>
2023-08-16 11:32:44 -0400

    updates

--- src/Store.js
+++ src/Store.js
@@ -1,37 +1,59 @@
 // store.js
-import { createStore } from 'vuex';
-import axios from 'axios';
+import { createStore } from "vuex";
+import axios from "axios";
 
 const store = createStore({
   state() {
     return {
-      apiData:{},
+      apiData: {},
+      tasks: [],
     };
   },
   mutations: {
     setApiData(state, apiData) {
-        state.apiData = apiData;
+      state.apiData = apiData;
+    },
+    addUser(state, newUser) {
+      state.apiData.push(newUser)
+      localStorage.setItem("apiData", JSON.stringify(state.apiData));
+    },
+    setTasks(state, tasks) {
+      state.tasks = tasks;
     },
   },
   actions: {
-     async fetchDataFromApi(context) {
-        try {
-          const response = await axios.get('https://reqres.in/api/users?page=1');
-          context.commit('setApiData', response.data);
-          console.log(response.data)
-          localStorage.setItem('apiData', JSON.stringify(response.data));
-        } catch (error) {
-          // just in case the API has a requestLimit like MarvelApi Lol
-  
-          console.error('Error fetching data:', error);
-          console.error('Backup was used due to Error:', error);
-          context.commit('setApiData', JSON.parse(localStorage.getItem('apiData')))
+    async fetchDataFromApi(context) {
+      try {
+        let localApiData =  JSON.parse(localStorage.getItem("apiData"))
+        if (localApiData) {
+          context.commit("setApiData", localApiData);
+          console.log(localApiData)
         }
+        else{
+          const response = await axios.get(
+            "https://reqres.in/api/users?page=1"
+          );
+          context.commit("setApiData", response.data);
+          localStorage.setItem("apiData", JSON.stringify(response.data));
+        }
+      } catch (error) {
+        // just in case the API has a requestLimit like MarvelApi Lol
+
+        console.error("Error fetching data:", error);
+        console.error("Backup was used due to Error:", error);
+        context.commit(
+          "setApiData",
+          JSON.parse(localStorage.getItem("apiData"))
+        );
+      }
     },
   },
   getters: {
     getApiData(state) {
-        return state.apiData;
+      return state.apiData;
+    },
+    getAllTasks(state) {
+      return state.tasks;
     },
   },
 });
--- src/views/ContactView/components/AddButton.vue
+++ src/views/ContactView/components/AddButton.vue
@@ -1,47 +1,97 @@
 <template>
-    
-   <div class="d-grid buttonDiv">
-    <button type="button" class="btn buttonComp m-4" data-bs-toggle="modal" data-bs-target="#staticBackdrop" title="Add Contact">
-        <i class="bi bi-person-plus-fill"></i>  Add
-    </button>
-   </div>
+    <div class="d-grid buttonDiv">
+        <button type="button" class="btn buttonComp m-4" data-bs-toggle="modal" data-bs-target="#staticBackdrop"
+            title="Add Contact">
+            <i class="bi bi-person-plus-fill"></i> Add
+        </button>
+    </div>
 
-    
+
     <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
         aria-labelledby="staticBackdropLabel" aria-hidden="true">
         <div class="modal-dialog">
-            <div class="modal-content">
+            <div class="modal-content bg-info-subtle">
                 <div class="modal-header">
-                    <h1 class="modal-title fs-5" id="staticBackdropLabel">Modal title</h1>
+                    <h1 class="modal-title fs-5" id="staticBackdropLabel">New Contact <i class="bi bi-person-circle"></i></h1>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
-                    ...
+                    <div class="modalForm">
+                        <img id="contactImg" :src="url" alt="profile">
+                        <div class="mb-3">
+                            <label for="formFile" class="form-label">Profile Picture</label>
+                            <input class="form-control" type="file" id="formFile" @change="fileHandler">
+                        </div>
+                    </div>
+                    <div class="modalForm">
+                        <div class="mb-3">
+                            <label for="exampleFormControlInput1" class="form-label">First Name</label>
+                            <input v-model="firstName" type="text" class="form-control" id="exampleFormControlInput1"
+                                placeholder="example: Petter">
+                        </div>
+                    </div>
+                    <div class="modalForm">
+                        <div class="mb-3">
+                            <label for="exampleFormControlInput1" class="form-label">Last Name</label>
+                            <input v-model="lastName" type="text" class="form-control" id="exampleFormControlInput1"
+                                placeholder="example: Mendoza">
+                        </div>
+                    </div>
+                    <div class="modalForm">
+                        <label>Choose your title</label>
+                        <select v-model="title" class="form-select" aria-label="Default select example">
+                            <option selected value="">Open this select menu</option>
+                            <option value="Polivalente">Polivalente</option>
+                            <option value="MultiRol">MultiRol</option>
+                            <option value="Garbage">Garbage</option>
+                            <option value="Jungla">Jungla</option>
+                            <option value="Top">Top</option>
+                            <option value="ADC">ADC</option>
+                            <option value="Support">Support</option>
+                            <option value="one Lane Specialist">one Lane Specialist</option>
+                            <option value="Mid">Mid</option>
+                        </select>
+                    </div>
+                    <div class="modalForm">
+                        <div class="mb-3">
+                            <label for="exampleFormControlInput1" class="form-label">PhoneNumber</label>
+                            <input v-model="phoneNumber" type="tel" class="form-control" id="exampleFormControlInput1"
+                                placeholder="example: 8092323423" maxlength="10">
+                        </div>
+                    </div>
+                    <div class="modalForm">
+                        <div class="mb-3">
+                            <label for="exampleFormControlInput1" class="form-label">Date of Birth</label>
+                            <input v-model="dateOfBirth" type="date" class="form-control" id="exampleFormControlInput1">
+                        </div>
+                    </div>
                 </div>
                 <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-primary">Understood</button>
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal"> <i class="bi bi-x-circle"></i>
+                        Close</button>
+                    <button type="button" class="btn btn-primary"><i class="bi bi-person-add"></i> Save</button>
                 </div>
             </div>
         </div>
     </div>
 </template>
 
-<script>
+<script setup>
 
 </script>
 
 <style scoped>
-.buttonDiv{
+.buttonDiv {
     place-items: center;
 }
-.buttonComp{
+
+.buttonComp {
     background-color: transparent;
     border: solid 2px black;
     transition: 1s;
 }
-.buttonComp:hover{
+
+.buttonComp:hover {
     background-color: blueviolet;
     color: white;
-}
-</style>
+}</style>
